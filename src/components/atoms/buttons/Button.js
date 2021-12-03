779095dbfa55e747b2187455a725ba0addedcab9
Muhammad <muhammad.rizwan@gmail.com>
2021-12-03 12:43:46 +0500

    create header component (login,withoutLogin), footer component and add some chage in checkbox and radiobutton

--- src/components/atoms/buttons/Button.js
+++ src/components/atoms/buttons/Button.js
@@ -10,7 +10,7 @@ function Button({
   style,
   type = 'contained',
   color = 'primary',
-  transform = '',
+  transform = "capitalize",
   ...otherProps
 }) {
   const btnClasses = useMemo(() => {
