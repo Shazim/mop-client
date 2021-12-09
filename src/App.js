4590fbca19c27d3c126457dc603ef341cc031987
Muhammad Rizwan <88097975+hrizwan44@users.noreply.github.com>
2021-12-09 17:13:04 +0500

    remove dead code from App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import { Provider } from 'react-redux';
 import { useStore } from '../src/store';
 import authRouter from 'routes/auth';
 import './index.css';
+
 function App() {
   const { store } = useStore();
 
   return (
-    <>
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            {authRouter.map((item) => (
-              <Route
-                exact
-                key={item.path}
-                path={item.path}
-                component={item.component}
-              />
-            ))}
-          </Switch>
-        </Router>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          {authRouter.map((item) => (
+            <Route
+              exact
+              key={item.path}
+              path={item.path}
+              component={item.component}
+            />
+          ))}
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
