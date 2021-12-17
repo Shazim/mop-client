d5293efc0eb5233ae33d395e5a04e8e0a9866b2c
Muhammad Rizwan <88097975+hrizwan44@users.noreply.github.com>
2021-12-17 19:34:42 +0500

    set and clear filter on side bar with login

--- src/components/atoms/checkbox/CheckBox.js
+++ src/components/atoms/checkbox/CheckBox.js
@@ -6,7 +6,7 @@ function CheckBox({ value, checked, onChange }) {
     return (
 
         <div className="flex flex-row w-100 cursor-pointer"  >
-            <label className="checkbox-container font-bold tracking uppercase">{value}
+            <label className="checkbox-container font-bold text-secondary tracking uppercase">{value}
                 <input type="checkbox" checked={checked} onChange={onChange} />
                 <span className="checkbox-checkmark"></span>
             </label>
--- src/components/atoms/sidebar/SideBarWithLogin.jsx
+++ src/components/atoms/sidebar/SideBarWithLogin.jsx
@@ -6,10 +6,14 @@ import Menu from '../menu/Menu'
 import SearchBar from '../searchbar/SearchBar'
 import Button from '../buttons/Button'
 import RadioMenu from '../menu/RadioMenu '
+import Input from 'components/app/common/Input'
 
 
 function SideBarWithLogin() {
     const [isOpen, setIsOpne] = useState(false)
+    const [minPrice, setMinPrice] = useState("")
+    const [maxPrice, setMaxPrice] = useState("")
+    const [search, setSearch] = useState("")
     const [filter, setFilter] = useState({
         "menu": [
             { name: "item1", select: false },
@@ -21,9 +25,14 @@ function SideBarWithLogin() {
             { name: "item2", select: false },
             { name: "item3", select: false }
         ],
-
+        "minPrice": "",
+        "search": "",
+        "maxPrice": ""
     })
 
+    useEffect(() => {
+        handlePrice()
+    }, [minPrice, maxPrice, search])
 
     const clickHandler = () => setIsOpne(!isOpen)
 
@@ -32,24 +41,40 @@ function SideBarWithLogin() {
         const items = [...filters[item]]
         items[index].select = !items[index].select;
         setFilter(filters)
-        // debugger
-
     }
 
     const clearFilter = () => {
         Object.entries(filter).map(([key, value]) => {
-            value.map((item, index) => {
-                const filters = { ...filter }
-                const filterItem = [...filter[key]]
-                filterItem[index].select = false
-                setFilter(filters)
-            })
+            if (typeof value !== "string") {
+                value.map((item, index) => {
+                    const filters = { ...filter }
+                    const filterItem = [...filter[key]]
+                    filterItem[index].select = false
+                    setFilter(filters)
+                })
+            } else {
+                filter[key] = ""
+                setMinPrice("")
+                setMaxPrice("")
+                setSearch("")
+                // console.log("ITEMMM  ", filter.minPrice)
+                setFilter(filter)
+
+            }
         })
+    }
+
+    const handlePrice = () => {
+        filter["minPrice"] = minPrice
+        filter["maxPrice"] = maxPrice
+        filter["search"] = search
+        console.log("FILTER ", filter)
+        setFilter(filter)
     }
 
     return (
         <div className="h-screen w-294 bg-gray-lighter pt-40 overflow-auto">
-            <SearchBar className="w-220 h-32 mx-auto mb-25" placeholder="search" transform="uppercase" />
+            <SearchBar className="w-220 h-32 mx-auto mb-25" placeholder="search" transform="uppercase" onChange={(e) => setSearch(e.target.value)} value={search} />
             {/* <Menu data={object} setData={setItem} />
             <div className=" h-1 bg-gray mx-36 my-16"></div>
             <Menu data={object} setData={handler} />
@@ -59,13 +84,15 @@ function SideBarWithLogin() {
             <Menu data={object} setData={handler} />
             <div className=" h-1 bg-gray mx-36 my-16"></div> */}
             {Object.entries(filter).map(([key, value]) => {
-                return (key !== "Radio Menu") ? <>
-                    <Menu data={value} title={key} setData={handler} />
-                    <div className=" h-1 bg-gray mx-36 my-16"></div>
-                </> : <>
-                    <RadioMenu data={value} title={key} setData={handler} />
-                    <div className=" h-1 bg-gray mx-36 my-16"></div>
-                </>
+                if (typeof value !== "string") {
+                    return (key !== "Radio Menu") ? <>
+                        <Menu data={value} title={key} setData={handler} />
+                        <div className=" h-1 bg-gray mx-36 my-16"></div>
+                    </> : <>
+                        <RadioMenu data={value} title={key} setData={handler} />
+                        <div className=" h-1 bg-gray mx-36 my-16"></div>
+                    </>
+                }
             })}
             <div className="relative">
                 <div className="flex justify-between px-36">
@@ -74,9 +101,23 @@ function SideBarWithLogin() {
                 </div>
                 <div className={`pt-10 px-36 ${isOpen ? "" : "hidden"}`} >
                     <div className="font-bold text-sm tracking uppercase text-secondary leading-8">MINIMUM</div>
-                    <FormField placeholder="enter price" className="focus:outline-none uppercase w-220 h-32 font-bold text-sm pl-10 text-secondary" />
+                    <Input
+                        name="minPrice"
+                        type="number"
+                        placeholder="enter price"
+                        className="textfield focus:outline-none uppercase w-220 h-32 font-bold text-sm pl-10 text-secondary"
+                        onChange={(text) => setMaxPrice(text.target.value)}
+                        value={maxPrice} />
+
                     <div className="font-bold text-sm tracking uppercase text-secondary mt-10 leading-8">MAXIMUM</div>
-                    <FormField placeholder="enter price" className="focus:outline-none uppercase w-220 h-32 font-bold text-sm pl-10 text-secondary" />
+                    <Input
+                        name="maxPrice"
+                        type="number"
+                        className="textfield focus:outline-none uppercase w-220 h-32 font-bold text-sm pl-10 text-secondary bg-white"
+                        placeholder="enter price"
+                        onChange={(text) => setMinPrice(text.target.value)}
+                        value={minPrice}
+                    />
                 </div>
             </div>
             <Button
