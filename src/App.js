62c378380e445f629c4f331810784a0c44cddc3c
Muhammad Rizwan <88097975+hrizwan44@users.noreply.github.com>
2021-12-21 12:38:54 +0500

    side bar filters completed

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import { Provider } from 'react-redux';
 import { useStore } from '../src/store';
 import authRouter from 'routes/auth';
 import './index.css';
+import SideBarWithLogin from 'components/atoms/sidebar/SideBarWithLogin';
+import SideBar from 'components/atoms/sidebar/SideBar';
 
 function App() {
   const { store } = useStore();
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          {authRouter.map((item) => (
-            <Route
-              exact
-              key={item.path}
-              path={item.path}
-              component={item.component}
-            />
-          ))}
-        </Switch>
-      </Router>
-    </Provider>
+    <>
+
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            {authRouter.map((item) => (
+              <Route
+                exact
+                key={item.path}
+                path={item.path}
+                component={item.component}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </Provider>
+
+    </>
   );
 }
 
