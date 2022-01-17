2313375ec45931c85aecd8f7f98ad1619e38d301
Muhammad Rizwan <88097975+hrizwan44@users.noreply.github.com>
2022-01-17 16:40:38 +0500

    tables convert into single component

--- src/components/atoms/tables/FilterTable.jsx
+++ src/components/atoms/tables/FilterTable.jsx
@@ -1,48 +1,57 @@
 import React from 'react'
 
-function FilterTable() {
+function FilterTable({ filter = true }) {
+
+    const obj = [
+        {
+            "name": "Mr Dean Horlock",
+            "customer since": "12/10/21",
+            "last purchase": "190.00",
+            "total purchases": "1,365"
+        },
+        {
+            "name": "Mr Dean Horlock",
+            "customer since": "12/10/21",
+            "last purchase": "190.00",
+            "total purchases": "1,365"
+        },
+        {
+            "name": "Mr Dean Horlock",
+            "customer since": "12/10/21",
+            "last purchase": "190.00",
+            "total purchases": "1,365"
+        },
+        {
+            "name": "Mr Dean Horlock",
+            "customer since": "12/10/21",
+            "last purchase": "190.00",
+            "total purchases": "1,365"
+        },
+    ]
+
     return (
+
+
         <div className='w-full bg-backgroundColor'>
             <table className='w-full'>
-                <tr className='h-42 tracking-wider font-avenir-reg text-base uppercase border-b border-gray bg-white'>
-                    <td className='pl-18'><div className='flex leading-5'>name <img className='-mt-6' src='/images/table/nameFilter.svg' /></div></td>
-                    <td className='pl-18 text-center'><div className='flex'>customer since <img src='/images/table/filterIcon.svg' /></div></td>
-                    <td className='pl-18'><div className='flex'>last purchase <img src='/images/table/filterIcon.svg' /></div></td>
-                    <td className='pl-18'><div className='flex'>total purchases <img src='/images/table/filterIcon.svg' /></div></td>
-                    <td className='pl-18'></td>
+                <tr className='h-42 tracking-wider font-avenir-reg text-base uppercase border-b-2 border-gray bg-white'>
+                    {Object.keys(obj[0]).map((keys, i) => {
+                        return <th className='pl-18'><div className='flex leading-5 text-secondary'>{keys} {filter ? <img className='-mt-6' src={`${i == 0 ? '/images/table/nameFilter.svg' : "/images/table/filterIcon.svg"}`} /> : ""}</div></th>
+                    })}
+                    <th></th>
                 </tr>
-                <tr className='h-60 border-b capitalize text-base font-reg text-black border-gray '>
-                    <td className='pl-18'>Mr dean horlok</td>
-                    <td className='pl-5%'>12/10/21</td>
-                    <td className='pl-5%'>&#163;190.00</td>
-                    <td className='pl-5%'>&#163;1,365</td>
-                    <td className=''><img src='/images/table/downArrow.svg' /></td>
-
-                </tr>
-                <tr className='h-60 text-black capitalize text-base font-reg border-b border-gray'>
-                    <td className='pl-18'>Mr dean horlok</td>
-                    <td className='pl-5%'>12/10/21</td>
-                    <td className='pl-5%'>&#163;190.00</td>
-                    <td className='pl-5%'>&#163;1,365</td>
-                    <td className=''><img src='/images/table/downArrow.svg' /></td>
-                </tr>
-                <tr className='h-60 border-b capitalize text-base font-reg text-black border-gray '>
-                    <td className='pl-18'>Mr dean horlok</td>
-                    <td className='pl-5%'>12/10/21</td>
-                    <td className='pl-5%'>&#163;190.00</td>
-                    <td className='pl-5%'>&#163;1,365</td>
-                    <td className=''><img src='/images/table/downArrow.svg' /></td>
-
-                </tr>
-                <tr className='h-60 text-black capitalize text-base font-reg border-b border-gray'>
-                    <td className='pl-18'>Mr dean horlok</td>
-                    <td className='pl-5%'>12/10/21</td>
-                    <td className='pl-5%'>&#163;190.00</td>
-                    <td className='pl-5%'>&#163;1,365</td>
-                    <td className=''><img src='/images/table/downArrow.svg' /></td>
-                </tr>
+                {obj.map((keys) => (
+                    <tr className='h-60 border-b capitalize text-base font-reg text-black border-gray '>{
+                        Object.values(keys).map((value, index) => (
+                            <td className={`${index == 0 ? "pl-18" : "pl-3%"}`}>{value}</td>
+                        ))
+                    }
+                        {filter ? <td className=''><img src='/images/table/downArrow.svg' /></td> : <td className='tracking text-xl'>...</td>}
+                    </tr>
+                ))
+                }
             </table>
-        </div>
+        </div >
     )
 }
 
