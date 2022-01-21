e8d805e0f753c2f1cacd3923b3c4499aa6770bbd
Muhammad Rizwan <88097975+hrizwan44@users.noreply.github.com>
2022-01-21 17:08:01 +0500

    replace css property to tailwind property

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
         xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
         '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        '4xl': '-477px 0 0 470px #c71118',
         inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
         none: 'none',
       },
