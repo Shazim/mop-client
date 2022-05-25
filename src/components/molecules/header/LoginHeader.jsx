7285b384d3e2caf5719a1f0d1cfc8692d86dff6e
abdulhanan145 <han1lgu.1212@gmail.com>
2022-05-25 16:43:46 +0500

    changes in mobile header complete design

--- src/components/molecules/header/LoginHeader.jsx
+++ src/components/molecules/header/LoginHeader.jsx
@@ -1,62 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from 'routes';
-import SearchBar from 'components/atoms/searchbar/SearchBar';
 
-import { slide as Menu } from 'react-burger-menu';
 import NavLinks from './NavLinks';
 import MobileNav from './MobileNav';
 
 function LoginHeader() {
-  const [open, setOpen] = useState(false);
-  console.log('here', open);
-  return (
-    <div className="py-32 flex justify-between max-screen  sm:w-full sm:relative">
-      <div className="w-30% ">
-        <Link to={routes.ROUTE_HOME}>
-          <img
-            src="/images/Logo/logo.svg"
-            alt=""
-            className="mr-25 w-onClick={() => setOpen(!open)}132 h-32"
-          />
-        </Link>
-      </div>
-      {/* <div
-        className="hidden sm:block absolute right-10%  link"
-        onClick={() => setOpen(!open)}
-      >
-        <img src="images/icons/menu.svg" alt="" />
-      </div> */}
-
-      <NavLinks />
-      <Menu
-        right
-        width={'100%'}
-        customBurgerIcon={
-          <img src="images/icons/menu.svg" className="hidden sm:block" />
-        }
-        onClick={() => setOpen(!open)}
-        customCrossIcon={<img src="images/icons/close.svg" />}
-        className="top-0"
-      >
-        <MobileNav />
-      </Menu>
-      {/* <div className="hidden sm:block sm:flex flex-col">
-        <Menu
-          right
-          width={'100%'}
-          customBurgerIcon={<img src="images/icons/menu.svg" />}
-          onClick={() => setOpen(!open)}
-        >
-          <NavLinks
-            className={
-              ''
-            }
-          />
-        </Menu>
-      </div> */}
-    </div>
-  );
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 960) {
+      setIsMobile(true);
+    }
+  }, []);
+  return isMobile ? <MobileNav /> : <NavLinks />;
 }
 
 export default LoginHeader;
--- src/components/molecules/header/MobileNav.jsx
+++ src/components/molecules/header/MobileNav.jsx
@@ -1,45 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { routes } from 'routes';
 import SearchBar from 'components/atoms/searchbar/SearchBar';
+import { slide as Menu } from 'react-burger-menu';
 
 const MobileNav = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className=" hidden sm:flex  sm:flex-col sm:bg-gray-lighter sm:w-100% sm:h-100% ">
-        {/* <div> */}
-        <SearchBar
-          className="flex items-center sm:mt-30 "
-          placeholder="Search Artist"
-          bgColor="bg-transparent"
-        />
-        {/* </div> */}
-        <Link to={routes.ROUTE_GALLERY_SETTING}>
-          <div
-            className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mt-30 `}
-          >
-            my gallery
-          </div>
-        </Link>
-        <Link to={routes.ROUTE_STORE}>
-          <div
-            className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mt-30 `}
-          >
-            store
-          </div>
-        </Link>
-        <div
-          className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mt-30`}
-        >
-          get started
+      <div className="py-32 flex justify-between max-screen  sm:w-full sm:relative">
+        <div className="w-30% ">
+          <Link to={routes.ROUTE_HOME}>
+            <img
+              src="/images/Logo/logo.svg"
+              alt=""
+              className="mr-25 w-onClick={() => setOpen(!open)}132 h-32"
+            />
+          </Link>
         </div>
-        <Link to={routes.ROUTE_MY_PROFILE}>
-          <div
-            className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mb-100% sm:mt-30`}
-          >
-            profile
+        <Menu
+          right
+          width={'100%'}
+          customBurgerIcon={
+            <img src="images/icons/menu.svg" className="hidden sm:block" />
+          }
+          onClick={() => setOpen(!open)}
+          customCrossIcon={
+            <img src="images/icons/close.svg" className="hidden sm:block" />
+          }
+          className="top-0"
+        >
+          <div className=" hidden sm:flex  sm:flex-col sm:bg-gray-lighter sm:w-100% sm:h-100% ">
+            {/* <div> */}
+            <SearchBar
+              className="flex items-center sm:mt-30 "
+              placeholder="Search Artist"
+              bgColor="bg-transparent"
+            />
+            {/* </div> */}
+            <Link to={routes.ROUTE_GALLERY_SETTING}>
+              <div
+                className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mt-30 `}
+              >
+                my gallery
+              </div>
+            </Link>
+            <Link to={routes.ROUTE_STORE}>
+              <div
+                className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mt-30 `}
+              >
+                store
+              </div>
+            </Link>
+            <div
+              className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking sm:mt-30`}
+            >
+              get started
+            </div>
+            <Link to={routes.ROUTE_MY_PROFILE}>
+              <div
+                className={`font-bold mt-6 ${'text-secondary'} text-sm uppercase hover:text-primary link tracking  sm:mt-30`}
+              >
+                profile
+              </div>
+            </Link>
           </div>
-        </Link>
+        </Menu>
       </div>
     </>
   );
