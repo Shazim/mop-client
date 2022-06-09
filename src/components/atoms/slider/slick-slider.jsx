ee8008b2c297e232062fcd0c4fd143ee897ca4de
abdulhanan145 <han1lgu.1212@gmail.com>
2022-06-09 18:38:24 +0500

    slick conditions of arrows and next buttons

--- src/components/atoms/slider/slick-slider.jsx
+++ src/components/atoms/slider/slick-slider.jsx
@@ -4,25 +4,31 @@ import 'slick-carousel/slick/slick-theme.css';
 import Arrow from './Arrow/Arrow';
 import { useFetch, usePost } from 'hooks';
 import { galleryMock } from 'api';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 const SlickSlider = () => {
-  // const images = [
-  //   { image: '/images/gallery-mockup/scene1-dark.png' },
-  //   { image: '/images/gallery-mockup/scene2-dark.png' },
-  // ];
+  const [currentSlide, setCurrentSlide] = useState(0);
 
+  const slider = useRef();
+
+  const next = () => {
+    slider.current.slickNext();
+  };
   const params = useParams();
+
   const settings = {
     dots: false,
-    // infinite: true,
+
     slidesToShow: 1,
+
     slidesToScroll: 1,
     nextArrow: <Arrow dir="right" type="next" />,
     prevArrow: <Arrow dir="left" type="prev" />,
-    // arrows: false,
-
+    arrows: currentSlide == 0 ? false : true,
+    afterChange: (indexOfCurrentSlide) => {
+      setCurrentSlide(indexOfCurrentSlide);
+    },
     responsive: [
       {
         breakpoint: 959,
@@ -32,14 +38,15 @@ const SlickSlider = () => {
           centerMode: true,
           dots: true,
           centerPadding: '0%',
-          transformEnabled: true,
-          vertical: true,
-          verticalSwaping: true,
+          //transformEnabled: true,
+          // vertical: true,
+          // verticalSwaping: true,
           swipeToSlide: true,
         },
       },
     ],
   };
+
   const width = window.innerWidth;
   const height = window.innerHeight;
 
@@ -73,13 +80,14 @@ const SlickSlider = () => {
   const backgroundImage = `${exhibition_detail?.style?.toLowerCase()}.png`;
   let a = 0;
   let b = 0;
+
   return (
     <>
       {width < 700 ? (
         <></>
       ) : width > height ? (
         <div className="w-100% h-100vh slider-dots">
-          <Slider {...settings}>
+          <Slider ref={(c) => (slider.current = c)} {...settings}>
             <div className="relative">
               <img
                 className="w-100% h-100vh  sm:w-100% sm:h-100%"
@@ -97,7 +105,11 @@ const SlickSlider = () => {
                   {exhibition_detail?.gallery_name}
                 </div>
                 <div className="flex mt-199">
-                  <img src="/images/arrows/arrow-mockup.svg" alt="" />
+                  <img
+                    src="/images/arrows/arrow-mockup.svg"
+                    alt=""
+                    onClick={next}
+                  />
                   <div className="ml-22 text-20 text-gray-lighter font-nunito-bold tracking uppercase">
                     Enter Gallery
                   </div>
@@ -196,44 +208,6 @@ const SlickSlider = () => {
                   </>
                 );
               })}
-
-            {/* {Array.from(Array(totalScene3Length + 1).keys())
-              .slice(1)
-              .map((index) => (
-                <div className="relative">
-                  <img
-                    className='className="w-100% h-100vh '
-                    src={backgroundImage}
-                    alt=""
-                  />
-                  <div className="absolute top-51%  transform-y flex w-100% justify-evenly">
-                    <div className="h-222 relative mr-20 ml-30  ">
-                      <img
-                        src="/images/gallery-mockup/portrait-frame.png"
-                        className="h-100% w-165 "
-                        alt=""
-                      />
-                      <img
-                        src="/images/gallery-mockup/dummy.png"
-                        alt=""
-                        className="absolute object-cover top-19 h-183 left-19 w-127  "
-                      />
-                    </div>
-                    <div className="h-222 relative mr-30  ">
-                      <img
-                        src="/images/gallery-mockup/portrait-frame.png"
-                        className="h-100% w-165 "
-                        alt=""
-                      />
-                      <img
-                        src="/images/gallery-mockup/dummy2.jpg"
-                        alt=""
-                        className="absolute top-19 h-183 left-19 w-127 object-cover"
-                      />
-                    </div>
-                  </div>
-                </div>
-              ))} */}
           </Slider>
         </div>
       ) : (
