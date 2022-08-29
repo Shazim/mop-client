7898997bf53e2e9ec0227eb505968ca18d07d887
Hassan Hafeez <hasancheeta@gmail.com>
2022-08-29 16:19:22 +0500

    fixed dropdown close on clikcing empty space and refactored some code to display select options

--- src/components/atoms/form/SelectOptions.jsx
+++ src/components/atoms/form/SelectOptions.jsx
@@ -20,44 +20,42 @@ function SelectOptions({
   const [text, setText] = useState(label);
   const [hide, setHide] = useState(true);
   const [value, setValue] = useState(0);
-  const displayItems = (name, value) => {
+
+  const handleSelect = (item, name) => {
+    onChange?.(item.value, item.label, name);
+    setText(item.label);
+    setValue(item.value);
     setHide(!hide);
-    setText(name);
-    setValue(value);
   };
+
   return (
-    <div
-      class={`relative ${classes} flex flex-row-reverse  h-32  cursor-pointer`}
-      onClick={() => setHide(!hide)}
-    >
-      <select className="hidden">
-        {option.map((item) => (
-          <option value={item.value}>{item.label}</option>
-        ))}
-      </select>
-      <div class="custom-arrow min-w-36 h-32 relative bg-gray-darkest"></div>
+    <OutsideAlerter close={() => setHide(true)}>
       <div
-        class={`select-selected text-${textColor} py-8 px-22 sm:px-10 w-100% tracking bg-${color} leading-4 font-bold text-sm sm:text-tiny sm:text-11 select-arrow-active uppercase`}
+        class={`relative ${classes} flex flex-row-reverse  h-32  cursor-pointer`}
+        onClick={() => setHide(!hide)}
       >
-        {text}
+        <select className="hidden">
+          {option.map((item) => (
+            <option value={item.value}>{item.label}</option>
+          ))}
+        </select>
+        <div class="custom-arrow min-w-36 h-32 relative bg-gray-darkest"></div>
+        <div
+          class={`select-selected text-${textColor} py-8 px-22 sm:px-10 w-100% tracking bg-${color} leading-4 font-bold text-sm sm:text-tiny sm:text-11 select-arrow-active uppercase`}
+        >
+          {text}
+        </div>
+        <div
+          class={`${
+            hide ? 'hidden' : ''
+          } font-bold uppercase select-items w-100% absolute top-100% text-secondary text-sm pl-22 left-0 tracking right-0 z-50 bg-gray-lightest leading-7`}
+        >
+          {option.map((item) => (
+            <div onClick={() => handleSelect(item, name)}>{item.label}</div>
+          ))}
+        </div>
       </div>
-      <div
-        class={`${
-          hide ? 'hidden' : ''
-        } font-bold uppercase select-items w-100% absolute top-100% text-secondary text-sm pl-22 left-0 tracking right-0 z-50 bg-gray-lightest leading-7`}
-      >
-        {option.map((item) => (
-          <div
-            onClick={() => {
-              onChange && onChange(item.value, item.label, name);
-              displayItems(item.label, item.value);
-            }}
-          >
-            {item.label}
-          </div>
-        ))}
-      </div>
-    </div>
+    </OutsideAlerter>
   );
 }
 
