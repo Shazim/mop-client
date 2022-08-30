3cfe569730c225a8f1230f2329cb11f097f9c34a
abhannan123 <abdul.hannan@orbiqon.com>
2022-08-30 16:15:57 +0500

    truncate class added in it

--- src/components/ImageList/index.jsx
+++ src/components/ImageList/index.jsx
@@ -23,7 +23,7 @@ const ImageList = ({
       <div className="flex items-center">
         <img className="w-29 h-29 rounded-sm mr-15" src={imageUrl} />
         <p
-          className=" w-200 font-bold tracking leading-32 text-secondary uppercase text-sm overflow-hidden text-ellipsis block whitespace-nowrap ...."
+          className=" w-200 font-bold tracking leading-32 text-secondary uppercase text-sm truncate"
           title={name}
         >
           {name}
