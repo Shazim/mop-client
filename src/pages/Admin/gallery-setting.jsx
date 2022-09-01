ee6e87895aaa910ac9936884676c52cbb0ed99bd
abhannan123 <abdul.hannan@orbiqon.com>
2022-09-01 20:21:47 +0500

    gallery setting page design issue fixes

--- src/pages/Admin/gallery-setting.jsx
+++ src/pages/Admin/gallery-setting.jsx
@@ -154,7 +154,7 @@ const GallerySetting = () => {
                   onClick={() =>
                     setFieldValue('gallery_type', 'private_gallery')
                   }
-                  className="w-193 h-32 sm:w-142 mr-32  sm:mr-10 sm:leading-18 sm:h-49 mt-0 uppercase "
+                  className="w-193 h-32 sm:w-142 mr-32 sm:px-24 sm:mr-10 sm:leading-18 sm:h-49 mt-0 uppercase "
                 >
                   Private Gallery
                 </ToggleButton>
@@ -172,23 +172,35 @@ const GallerySetting = () => {
                   onClick={() =>
                     setFieldValue('gallery_type', 'public_gallery')
                   }
-                  className=" w-193 h-32 sm:w-142 sm:leading-18 sm:h-49 mt-0 uppercase"
+                  className=" w-193 h-32 sm:w-142 sm:px-24 sm:leading-18 sm:h-49 mt-0 uppercase"
                 >
                   Public Gallery
                 </ToggleButton>
               </div>
               <div className="hr-b sm:hidden"></div>
             </div>
-            <div className="hr-b pb-31 pt-25 sm:hidden">
-              <div className="admin-h2 pb-5 ">Domain</div>
-              <TextField
-                name="domain"
-                placeholder="if you don’t have domain name, we will create one automatically for you"
-                mb="6"
-                label="if you don’t have domain name, we will create one automatically for you
+            <div className="hr-b pb-31 pt-25 ">
+              <div className="admin-h2 pb-5 sm:pb-18">Domain</div>
+              <div className="sm:hidden">
+                <TextField
+                  name="domain"
+                  placeholder="if you don’t have domain name, we will create one automatically for you"
+                  mb="6"
+                  label="if you don’t have domain name, we will create one automatically for you
                 "
-                className="w-55% h-38 sm:w-full"
-              />
+                  className="w-55% h-38 sm:hidden"
+                />
+              </div>
+              <div className="hidden sm:block mb-5">
+                <TextField
+                  name="domain"
+                  placeholder="TYPE YOUR DOMAIN HERE"
+                  mb="6"
+                  label="type in a domain name:
+                "
+                  className="hidden sm:h-38 sm:block sm:w-full"
+                />
+              </div>
             </div>
             <div className="flex  pt-20 hr-b pb-29 sm:flex-col ">
               <div>
