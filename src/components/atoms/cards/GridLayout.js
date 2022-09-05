01152e9d8a785381e9d27ad682b084d3202037dc
Hassan Hafeez <hasancheeta@gmail.com>
2022-09-05 16:52:36 +0500

    fixed featured artworks api

--- src/components/atoms/cards/GridLayout.js
+++ src/components/atoms/cards/GridLayout.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { useFetch } from 'hooks';
 import '../../../index.css';
 import { featureArtwork } from 'api/api-services';
+import { parsePath } from 'history';
 
 export default function GridLayout({
   column,
   gap,
   obj = [
     {
-      imageUrl: '/images/gallery/image0.png',
+      imageUrl: `${window.location.origin}/images/gallery/image0.png`,
       userName: 'Amy Moore',
       status: 'available',
       userImage: '/images/gallery/userImage.png',
@@ -43,22 +44,21 @@ export default function GridLayout({
 }) {
   const { data: getData } = useFetch(featureArtwork);
   const { featured_artworks } = getData || [];
-
+  console.log(featured_artworks);
   return (
     <div style={{ columns: column, columnGap: 12 }}>
       {featured_artworks &&
-        featured_artworks?.map((item, i) => (
-          <div className="relative sm:rounded-8">
+        featured_artworks?.map((item) => (
+          <div className="relative sm:rounded-8" key={item?.id}>
             <img
-              src={item.featured_image}
-              key={i}
+              src={item?.images[0]?.image}
               className="image sm:rounded-8 mb-12"
             />
             <div className="absolute bottom-20 sm:bottom-15 left-21">
               <div className="flex items-center mb-8 sm:mb-5">
                 <img
                   className="w-30 h-32 sm:w-24 sm:h-23 rounded-50%"
-                  src={item?.featured_image}
+                  src={item?.images[0]?.image}
                 />
                 <p className="text-white text-base font-avenir-reg leading-22 tracking-tight font-bold ml-10 sm:text-tiny">
                   {item?.name}
