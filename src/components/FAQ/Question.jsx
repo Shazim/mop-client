2dd123b81b0a92b92ceab9e6e10aed4240bc4140
abhannan123 <abdul.hannan@orbiqon.com>
2022-09-06 17:03:40 +0500

    add question and answers in faq section

--- src/components/FAQ/Question.jsx
+++ src/components/FAQ/Question.jsx
@@ -2,27 +2,40 @@ import React from 'react';
 import { useState } from 'react';
 import Answer from './Answer';
 
-function Question({ question = 'How many pictures can I store?' }) {
+function Question({
+  question = [
+    'How many pictures can I store?',
+    'How many pictures can I store?',
+    'How many pictures can I store?',
+    'How many pictures can I store?',
+    'How many pictures can I store?',
+  ],
+}) {
   const [showData, setShowData] = useState(false);
-  const handleClick = () => {
+  const [id, setId] = useState();
+
+  const handleClick = (i) => {
+    setId((prev) => (prev == i ? null : i));
     setShowData(!showData);
   };
   return (
     <div className="border-b border-border">
-      <div className="flex text-black font-nunito-light pt-20 pb-18 px-19 justify-between border-t  border-border text-base leading-22">
-        <div>{question}</div>
-        <img
-          src={
-            showData
-              ? '/images/icons/open-option.svg'
-              : '/images/icons/close-option.svg'
-          }
-          className="link"
-          alt=""
-          onClick={handleClick}
-        />
-      </div>
-      {showData && <Answer />}
+      {question.map((item, index) => (
+        <>
+          <div
+            className="flex text-black font-nunito-light pt-20 pb-18 px-19 justify-between border-t  border-border text-base leading-22 link"
+            onClick={() => handleClick(index)}
+          >
+            <div>{item}</div>
+
+            <img
+              className={`${id == index && 'transform rotate-180'}`}
+              src="/images/table/downArrow.svg"
+            />
+          </div>
+          {id == index && <Answer />}
+        </>
+      ))}
     </div>
   );
 }
