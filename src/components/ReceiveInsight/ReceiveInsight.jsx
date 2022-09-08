75428f08c015db16dc425000a08d2ef138a4c8c6
Hassan Hafeez <hasancheeta@gmail.com>
2022-09-08 17:13:43 +0500

    fixed bottom subscription section on homepage

--- src/components/ReceiveInsight/ReceiveInsight.jsx
+++ src/components/ReceiveInsight/ReceiveInsight.jsx
@@ -23,7 +23,8 @@ function ReceiveInsight({ image = '/images/girl.png' }) {
 
   return (
     <div className="flex border-border border-t sm:border-t-0 sm:bg-offwhite ">
-      <div className=" max-screen sm:px-23 sm:w-100% pt-101 w-40% xl:w-45% lg:w-50% md:w-50% xl:pb-50 lg:pb-50 md:pb-50">
+      {/* <div className=" max-screen sm:px-23 sm:w-100% pt-101 w-40% xl:w-45% lg:w-50% md:w-50% xl:pb-50 lg:pb-50 md:pb-50"> */}
+      <div className=" m-auto sm:px-23 sm:w-100% pt-30 w-40% xl:w-45% lg:w-50% md:w-50% xl:pb-50 lg:pb-50 md:pb-50">
         <div className="sm:w-100% sm:flex-col sm:flex sm:items-center sm:justify-center sm:pb-50">
           <div className="tracking-wider text-2xl sm:text-center sm:text-xl uppercase font-avenir-reg pb-34">
             Join our mailing list Subscribe for priority access to our latest
