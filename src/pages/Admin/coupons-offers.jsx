ec0b311ec642f21d5f9e58425a2bf93da8e8da65
abhannan123 <abdul.hannan@orbiqon.com>
2022-09-12 13:23:39 +0500

    set the heading of coupon and   offers tables and filter icons

--- src/pages/Admin/coupons-offers.jsx
+++ src/pages/Admin/coupons-offers.jsx
@@ -5,6 +5,7 @@ import Tables from 'components/molecules/tables/Tables';
 import SelectOptions from 'components/atoms/form/SelectOptions';
 import { AdminLayout } from 'Layout';
 import withArtistRoute from 'hoc/withArtistRoute';
+import { SIGNIN_SUCCESS } from 'store/actions/actionTypes';
 
 const CouponsOffers = () => {
   return (
@@ -13,7 +14,10 @@ const CouponsOffers = () => {
         <SelectOptions className="w-178" label="2020-2021" />
       </div>
       <div className="pt-28">
-        <Tables filter={true} />
+        <Tables
+          coupon={true}
+          tableHeading={['Name', 'Code', 'Active Since', 'Status']}
+        />
       </div>
     </AdminLayout>
   );
