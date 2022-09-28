97cd3a192970d1f213d3d7e0929cd7ecb2238e9b
abhannan123 <abdul.hannan@orbiqon.com>
2022-09-28 17:24:59 +0500

    here i implemented api of single exhibitioon

---
+++ src/pages/Admin/SingleExhibition.jsx
@@ -0,0 +1,90 @@
+import GalleryCard from 'components/atoms/cards/GalleryCard';
+import { AdminLayout } from 'Layout';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { getSingleExhibition } from 'api/api-services';
+import { useFetch } from 'hooks';
+import { routes } from 'routes';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const SingleExhibition = () => {
+  const { id } = useParams();
+  const { data: dataExhibition = [] } = useFetch(getSingleExhibition, {
+    variables: id,
+  });
+  const { artworks, image, room_name, key } = dataExhibition || [];
+  console.log({ dataExhibition });
+  const handleCopy = (key) => {
+    if (key) {
+      navigator.clipboard.writeText(key);
+      toast.info('Copied To Clipboard');
+    }
+  };
+  return (
+    <>
+      <AdminLayout title="Exhibition Details">
+        <div className="  max-screen mb-50">
+          <img
+            src={image || '/images/card/kelly-sikkema-tk9RQCq5eQo-unsplash.jpg'}
+            alt=""
+            className="exhition-pic  sm:h-290"
+          />
+        </div>
+        <div className="  w-100% pt-50   sm:px-0">
+          <div className="flex sm:flex-col-reverse">
+            <p className="font-avenir-reg text-2xl text-dark tracking-wider leading-60 uppercase flex-grow">
+              {room_name}
+            </p>
+            <div className="flex sm:mb-20 ">
+              <input
+                className="bg-gray-lighter h-33 w-100% outline-none text-center"
+                placeholder="exhibit.vip/gal/123456"
+                value={key}
+                readOnly={true}
+              />
+              <button
+                onClick={() => handleCopy(key)}
+                className="w-116 h-33 bg-primary  admin-label shadow uppercase"
+              >
+                <span className="text-white">Copy</span>
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="w-6 h-6 bg-primary rounded-full mt-5 mr-8"></div>
+            <div className="border-b-2 border-black w-100% pt-6"></div>
+          </div>
+          <p className="font-avenir-reg text-2xl text-primary tracking-wider leading-60 uppercase ">
+            Artworks
+          </p>
+          {artworks?.length > 0 && (
+            <div className="gridView  sm:grid grid-cols-1 mt-40 mb-80">
+              {artworks?.map(({ id, name, images }) => (
+                <>
+                  {images?.map(({ image, featured_image }) => (
+                    <>
+                      {featured_image && (
+                        <Link to={`${routes.ROUTE_SINGLE_STOCK}/${id}`}>
+                          <div className="mb-25">
+                            <GalleryCard
+                              imageUrl={image}
+                              title={name}
+                              className="stockroom__images"
+                            />
+                          </div>
+                        </Link>
+                      )}
+                    </>
+                  ))}
+                </>
+              ))}
+            </div>
+          )}
+        </div>
+      </AdminLayout>
+    </>
+  );
+};
+
+export default SingleExhibition;
